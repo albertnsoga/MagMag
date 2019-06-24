cd65f92b072b77fd829a3f657ba48a9ff793fef7
HP <beckalbert@ymail.com>
2019-06-24 18:16:06 +0200

    authentification réussie connexion en base de donnée réussie création de compte user réussie

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/magmag/NonNull.java
@@ -0,0 +1,4 @@
+package com.google.firebase.quickstart.auth.magmag;
+
+@interface NonNull {
+}
---
+++ app/src/main/java/com/google/firebase/quickstart/auth/magmag/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.google.firebase.quickstart.auth.magmag.Prevalent;
+
+
+import com.google.firebase.quickstart.auth.magmag.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
